64bde828d68c28e8b560a1dee5a28dfdc83a2882
Larry Myerscough <hippostech@gmail.com>
2018-04-04 15:28:06 +0200

    taking shape well. even re's are on my side today!

--- minder/base_test.py
+++ minder/base_test.py
@@ -1,57 +1,66 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Minder is a simple(ish) test framework ..
+Minder is a simple(ish) test and evaluation framework ..
 """
 import sys, os, subprocess
+from collections import OrderedDict
+from phileas import _html40 as h
+# import phileas
+# print(phileas.__path__)
 
 dbg_print = (int(os.getenv('MINDER_DBG', 0)) and print) or (lambda *pp, **kw: None)
 
 
 class BaseTest:
     exec_dir = ''  # default = find executables via path mechanism
-    exec_file = 'ping' #'echo'  # stub for initial testing
+    exec_file = 'ping'  # stub for initial testing
     showOut = True
     times_over = 1
+    count = 4
 
     def get_flavours(self):
-        return ( 'localhost', 'absenthost')
+        return 'localhost', 'absenthost'
 
     def get_args(self, flavour):
-        return ('-c', '4', flavour)
+        return '-c', '4', flavour
+
+    def arrange_args_for_table(self, flavour):
+        return 'width:40%', zip(('host', flavour),
+                             ('count', self.count))
 
     def cmd(self, *pp):
         print(pp)
         answer = (('stdbuf', '-o0', self.exec_dir + self.exec_file,)
-            + tuple(map(str, pp)))
+                  + tuple(map(str, pp)))
         dbg_print("External.cmd answer = ", answer)
         return answer
 
     def fixup(self, output, error):
         return output, error
 
-    def process_output(self, output):
+    def process_output(self, rc, output):  # obsolescent?
         pass  # stub
-
 
     def run(self, *args):
         cmd1 = self.cmd(*args)
-        dbg_print (cmd1)
+        dbg_print(cmd1)
         process = subprocess.Popen(cmd1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        rc = process.wait()
         output, error = (pipe.read().decode('utf8', errors='ignore')
                          for pipe in (process.stdout, process.stdout))
 
-        print ("stdout: \n", output)
-        print ("stderr: \n", error)
+        print(*["%s=<<%s>>\n" % (name, data) for name, data in
+              (('stdout', output),
+               ('stderr', error))])
         output, error = self.fixup(output, error)
-        return output
+        return rc, output
 
-    def inspect(self, flavour, output):
+    def inspect(self, flavour, rc, output):
         print("sorry, I - %s - don't (yet) do analysis!" % self)
         return False
 
     def summarize(self):
-        print("sorry, I - %s - don't (yet) do summarizing!" % self)
         return False
 
     def prepare(self):
@@ -64,22 +73,34 @@ class BaseTest:
             print("'%s' is not implemented or maybe just not enabled for this platform"
                   % self.__class__.__name__)
             return
-        self.rows = []
+        tables = OrderedDict()
         for time_over in range(self.times_over):
-            row = []
             for flavour in flavours:
-                output = self.run(*self.get_args(flavour))
-                column = self.inspect(flavour, output)
-                row.append(column)
-            self.rows.append(row)
+                if not time_over:
+                    style, args_nice = self.arrange_args_for_table(flavour)
+                    table = h.table(style=style)
+                    headers, values = (args_nice)
+                    table |= (h.tr() | [(h.th | header) for header in headers])
+                    table |= (h.tr() | [(h.td(style="color:blue;text-align:center") | value) for value in values])
+                    print(table, file=self.html_out)
+                    #sys.exit(42)  # temporary!
+                    tables[flavour] = table
+                table = tables[flavour]
+                rc, output = self.run(*self.get_args(flavour))
+
+                style, out_nice = self.inspect(flavour, rc, output)
+                # row.append(column)
+            # self.rows.append(row)
         self.summarize()
+        print(h.p | [v for v in tables.values()], file=self.html_out)
 
     def main(self):
         print("running %s" % sys.argv.pop(0))
         if sys.argv:
             self.run(*sys.argv)
         else:
-            self.exercise()
+            with open('test.html', 'w') as self.html_out:
+                self.exercise()
 
 
 if __name__ == "__main__":
--- minder/serial_test.py
+++ minder/serial_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import re
 from .base_test import BaseTest
 
 class SerialTest(BaseTest):
@@ -8,15 +9,28 @@ class SerialTest(BaseTest):
     showOut = True
     times_over = 2
     bit_rates = (115200, 250000, 500000, 1500000)
-    ttyPort = '/dev/ttyUSB0'
+    ttyPort = '/dev/ttyUSB1'
     tx_secs = 3  # 30
     rx_secs = 5  # 35
+
     def get_flavours(self):
         return self.bit_rates
 
     def get_args(self, flavour):
         return ('-p', self.ttyPort, '-o', self.tx_secs, '-i', self.rx_secs,
                 '-b', flavour)
+
+    def arrange_args_for_table(self, flavour):
+        return 'width:80%', zip(('ttyPort', self.ttyPort),
+                             ('bit ("baud") rate', flavour),
+                             ('transmit seconds', self.tx_secs),
+                             ('receive seconds', self.rx_secs),
+                             )
+
+    def inspect(self, flavour, rc, output):
+        result = re.search(r'session\:\s*rx=(\d+)\D+tx=(\d+)\D+rx\s*err=(\d+)',
+                           output)
+        print(result.groups())
 
 
 if __name__ == "__main__":
